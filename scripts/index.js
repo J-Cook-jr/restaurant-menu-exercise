4974579a56299eda60b04531a87b73aea8f1847b
Jeff Cook <jecook2692@gmail.com>
2020-04-23 10:57:30 -0400

    adds li function

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,11 @@
 function getCategories(menu){
-    return Object.keys(menu)
+    return Object.keys(menu);
 }
+
+function nameToListItem(categoryName){
+    const listItem = document.createElement('li');
+    listItem.textContent = categoryName;
+    return listItem;
+}
+
+console.log(nameToListItem(''));
